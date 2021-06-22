4dfaa33302ee25e787082eb37eadb1cf248516d4
= <kbooster17@gmail.com>
2021-06-22 05:05:31 +0000

    add homepage hero

--- src/containers/pages/HomePage.js
+++ src/containers/pages/HomePage.js
@@ -1,7 +1,7 @@
 import { Nav, Navbar } from "react-bootstrap";
 import "./HomePage.scss";
 import { Component } from "react";
-import ambulance from "../../assets/imges/ambulance.svg";
+import heart from "../../assets/imges/heart.svg";
 
 class HomePage extends Component {
   render() {
@@ -23,15 +23,30 @@ class HomePage extends Component {
               Home
             </Nav.Link>
           </Navbar>
-          <div className="hero-container">
-            <div className="hero-title-container">
-              <h2 className="hero-title">More than just a touch of care</h2>
-              <div className="hero-side-bg">
-                <img src={ambulance} alt="ambulance" className="ambu-img" />
+          <div className="hero-container d-flex">
+            <div className="hero-title-container mx-auto my-auto col-md-6">
+              <h1 className="hero-title fw-bold mx-3 my-4">More than just a  <span class="fw-bold text-success">touch</span> of care</h1>
+             <div className="hero-card shadow-lg d-flex mt-5 mx-auto">
+               <img src={heart} className="lil-heart"/>
+               <div className="hero-card-cont">
+                  24 hours Health-Care
+               </div>
+               <div className="hero-card-cont">
+                  7/7 days
+               </div>
+             </div>
+            </div> 
+            <div className="hero-side-bg col-md-6">
+                
               </div>
-            </div>
           </div>
         </header>
+
+        <section className="home-content">
+          <div className="services">
+
+          </div>
+        </section>
       </div>
     );
   }
